9925d424d6025fc2b8f1e33401ef7a5108e3477d
Hrushikesh-Reddy <hrushikesh1330@gmail.com>
2026-03-15 21:14:09 +0530

    added types

--- frontend/src/app/chat/ChatInput.tsx
+++ frontend/src/app/chat/ChatInput.tsx
@@ -8,19 +8,19 @@ export default function ChatInput({ onSubmit, onStop, loading }: { onSubmit: Fun
     //console.log(onSubmit)
     const [text, setText] = useState("");
 
-    function handleSubmit(e: React.InputEvent) {
+    function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
         e.preventDefault()
         if (text.trim() === "") return
         onSubmit(text)
         setText("")
     }
 
-    function handleStop(e: React.InputEvent) {
+    function handleStop(e: React.MouseEvent) {
         e.preventDefault()
         onStop()
     }
 
-    function handleKeyDown(e) {
+    function handleKeyDown(e: React.KeyboardEvent<HTMLTextAreaElement>) {
         if (e.key === "Enter" && text.trim() !== "") {
             e.preventDefault()
             onSubmit(text)
@@ -28,15 +28,15 @@ export default function ChatInput({ onSubmit, onStop, loading }: { onSubmit: Fun
         }
     }
 
-    function handleAddFiles(e) {
+    function handleAddFiles(e: React.MouseEvent) {
         e.preventDefault()
     }
 
     return (
-        <Form onSubmit={handleSubmit} action="" className=" bg-amber-50 rounded w-[65%] flex flex-col m-2 mb-0" >
-            <textarea onKeyDown={handleKeyDown} value={text} onChange={(e) => setText(e.target.value)} className="input-text  text-gray-800 text-wrap outline-0 p-2 m-2 resize-none" />
+        <Form onSubmit={(e) => handleSubmit(e)} action="" className=" bg-amber-50 rounded w-[65%] flex flex-col m-2 mb-0" >
+            <textarea onKeyDown={(e) => handleKeyDown(e)} value={text} onChange={(e) => setText(e.target.value)} className="input-text  text-gray-800 text-wrap outline-0 p-2 m-2 resize-none" />
             <div className="flex flex-row justify-between pt-0 mt-0 p-2 m-2">
-                <button onClick={handleAddFiles}>
+                <button onClick={e => handleAddFiles(e)}>
                     <Plus className="text-black hover:bg-amber-100 hover:rounded-full transition-all" />
                 </button>
                 {!loading ?
@@ -44,7 +44,7 @@ export default function ChatInput({ onSubmit, onStop, loading }: { onSubmit: Fun
                         <SendHorizontal className="text-black hover:bg-amber-100 hover:rounded-full transition-all" />
                     </button>
                     :
-                    <button onClick={handleStop} className="">
+                    <button onClick={e => handleStop(e)} className="">
                         <CircleStop className="text-black hover:bg-amber-100 hover:rounded-full transition-all" />
                     </button>
                 }
