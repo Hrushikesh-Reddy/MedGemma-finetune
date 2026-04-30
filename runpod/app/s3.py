854b478df86f815e2d25ba5a19692b65e21b16f9
Hrushikesh-Reddy <hrushikesh1330@gmail.com>
2026-04-30 10:17:20 +0530

    updated runpod worker

--- runpod/app/s3.py
+++ runpod/app/s3.py
@@ -9,15 +9,15 @@ AWS_SECRET_KEY=os.environ.get("AWS_SECRET_KEY")
 
 s3 = boto3.client(
     service_name="s3",
-    region_name= AWS_BUCKET_REGION,#os.environ.get("AWS_BUCKET_REGION"),
-    aws_access_key_id=AWS_ACCESS_KEY,#os.environ.get("AWS_ACCESS_KEY"),#
-    aws_secret_access_key=AWS_SECRET_KEY#os.environ.get("AWS_SECRET_KEY"),#,
+    region_name= AWS_BUCKET_REGION,
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY
 )
 
 def get_files(key: str):
     res = s3.get_object(
         Key=key,
-        Bucket = AWS_BUCKET_NAME # os.environ.get("AWS_BUCKET_NAME")#
+        Bucket = AWS_BUCKET_NAME
     )
     #print(res)
     return res
